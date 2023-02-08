62307cf7e5e7b0dd8197da018d37bed3b94b2f57
Lucas Rodrigues <lucasrodriguesbr12@gmail.com>
2023-02-08 13:07:52 -0300

    primeiro commit

---
+++ vitrine-back(2)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vitrine_back_2_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
